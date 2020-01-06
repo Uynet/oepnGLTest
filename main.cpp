029f260b7ad0dad9f73f76f500a692207acabe49
uynet <highsate@gmail.com>
2020-01-06 22:11:09 +0900

    fix/sh

--- main.cpp
+++ main.cpp
@@ -5,11 +5,6 @@
 #include <string>
 #include "renderer.h"
 
-static GLFWwindow *aWindow;
-static int aWidth = 640;
-static int aHeight = 480;
-static std::string aTitle = "Hello, OpenGL";
-
 int main()
 {
     Renderer renderer = Renderer();
