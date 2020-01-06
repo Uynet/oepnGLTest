7ab0249ffd048bac68f4744b2925f03af4e14cff
uynet <highsate@gmail.com>
2020-01-06 21:07:45 +0900

    ファイル分割

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include "renderer.h"
 
 static GLFWwindow *aWindow;
 static int aWidth = 640;
@@ -11,13 +12,13 @@ static std::string aTitle = "Hello, OpenGL";
 
 int main()
 {
-    // Initializes the GLFW library.
+    Renderer renderer = Renderer();
+    renderer.test();
     if (!glfwInit())
     {
         std::cerr << "glfwInit failed." << std::endl;
         exit(EXIT_FAILURE);
     }
-    // Creates a window and its associated context.
     aWindow = glfwCreateWindow(aWidth, aHeight, aTitle.c_str(), nullptr, nullptr);
     if (!aWindow)
     {
@@ -25,26 +26,16 @@ int main()
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
-    // Makes the context of the specified window current for the calling thread.
     glfwMakeContextCurrent(aWindow);
-    // Returns a string describing the compile-time configuration.
-    std::cout << glfwGetVersionString() << std::endl;
-    // glClear() で使用する色（RGBA）
+    std::cout << glfwGetVersionString() << std::endl; // Returns a string describing the compile-time configuration.
     glClearColor(1, 1, 1, 1);
 
-    // Checks the close flag of the specified window.
     while (!glfwWindowShouldClose(aWindow))
     {
-        // 画面をクリア。
         glClear(GL_COLOR_BUFFER_BIT);
-
-        // Swaps the front and back buffers of the specified window.
-        glfwSwapBuffers(aWindow);
-        // Processes all pending events.
-        // マウスやキー入力のイベントはこの関数で監視する。
-        glfwPollEvents();
+        glfwSwapBuffers(aWindow); // Swaps the front and back buffers of the specified window.
+        glfwPollEvents();         //input
     }
-    // Terminates the GLFW library.
     glfwTerminate();
 
     return EXIT_SUCCESS;
---
+++ renderer.cpp
@@ -0,0 +1,9 @@
+#include "renderer.h"
+#include <iostream>
+using namespace std;
+
+Renderer::Renderer() {}
+void Renderer::test()
+{
+    cout << "debugLog" << endl;
+}
